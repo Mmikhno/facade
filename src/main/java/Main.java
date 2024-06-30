feb092f738166412cece50ee4c5fc31a61783d95
Maria Mikhno <mmikhno89@gmail.com>
2024-06-30 14:17:48 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(Integer.toBinaryString(10));
+        System.out.println(Integer.toBinaryString(20));
+        System.out.println(Integer.toBinaryString(200));
+        System.out.println(binOps.mult("1010", "10100"));
+    }
+}
